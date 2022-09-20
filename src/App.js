fd027e8fc8faacd8d4ba65f46c4047434ea05562
Barbara <barbara160520@gmail.com>
2022-09-20 14:43:21 +0300

    DZ_2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import React, { useState } from "react";
 import Message from './Message';
 
 function App() {
-  let name = "Варвара";
+  const [messageList, setMessageList] = useState([]);
+  const [author, setName] = useState('');
+  const [text, setText] = useState('');
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+   
+    setMessageList( prevState => [...prevState,{
+      text: text,
+      author: author
+    }])
+  }
 
   return (
     <div>
         <h1>Добро пожаловать</h1>
-        <Message name={name} />
+          <form onSubmit={handelSubmit}>
+            <label>Ваше имя: </label>
+            <input value={author} onChange={(event)=>setName(event.target.value)}/>
+            <label>Ваше сообщение: </label>
+            <input  value={text} onChange={(event)=>setText(event.target.value)}/>
+            <button type='submit'>Отправить</button>
+          </form>
+          <Message messageList={messageList} setMessageList={setMessageList}/>
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,33 @@
 import './App.css';
+import React, { useEffect } from "react";
 
-function Message({name}) {
+function Message({messageList, setMessageList}) {
+
+  useEffect(() => {
+    setTimeout(() => {
+      robot()
+    },2000)
+  },[messageList])
+
+  function robot(){
+    const user = messageList[messageList.length - 1];
+    if(user && user.author!="Админа"){
+      setMessageList( prevState => [...prevState, {
+        author: 'Админа',
+        text: `Привет ${user.author}` 
+      }])
+    }
+  }
+
     return (
-      <div class="message">
-          {name}, Вам пришло сообщение!
+      <div>
+      {messageList.map((message => {
+        return(
+          <div class="message">
+            Чат: "{message.text}" от {message.author}
+          </div>
+        )
+      }))}
       </div>
     );
   }
